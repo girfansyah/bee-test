5169eec0a100f98caf2da6e813859160ffb3f3c7
Gilang Irfansyah <oadacode@gmail.com>
2022-08-29 21:35:07 +0700

    fix logic test1

--- logictest/test1.js
+++ logictest/test1.js
@@ -7,3 +7,11 @@ let words = 'beever';
 // beev
 // beeve
 // beever
+
+for (let i = 0; i <= words.length - 1; i++) {
+  let result = '';
+  for (let j = 0; j <= i; j++) {
+    result += words[j];
+  }
+  console.log(result);
+}
